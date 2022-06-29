70c638827e01951f76a9e6ddf88ae44fa9a77eef
Tatiane Guerra <tatiguerracosta30@gmail.com>
2022-06-29 09:08:48 -0300

    alteraçao com sucesso

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -1,55 +1,46 @@
 /// <reference types="cypress" />
-beforeEach(() => {
-    cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
-});
+const perfil = require('../fixtures/perfil.json')
 
-
-it('Deve adicionar produtos ao carrinho - Usando Comando customizado',()=> {
-    cy.addproduto('Abominable Hoodie', 'L', 'Red', 2)
-
-    cy.get('.woocommerce-message').should('contain' , '2 × “Abominable Hoodie” foram adicionados no seu carrinho')
-})
-it('Deve adicionar produtos ao carrinho - Usando Comando customizado',()=> {
-    cy.addproduto('Aero Daily Fitness Tee', 'XS', 'Brown', 2)
-
-    cy.get('.woocommerce-message').should('contain' , '2 × “Aero Daily Fitness Tee” foram adicionados no seu carrinho')
-})
-it('Deve adicionar produtos ao carrinho - Usando Comando customizado',()=> {
-    cy.addproduto('Aether Gym Pant', '32', 'Green', 2)
-
-    cy.get('.dropdown-toggle > .mini-cart-items')
-    cy.get('.woocommerce-message').should('contain' , '2 × “Aether Gym Pant” foram adicionados no seu carrinho')
-})
-
-
-/// <reference types="cypress" />
 import EnderecoPage from '../support/page_objects/nome-funcionliada.page'
-  const dadosEndereco= require('../fixtures/endereco.json')
+
 describe('funcionalidade Endereços - Faturamento e Entrega', () => {
- beforeEach (() => {
-     cy.visit('minha-conta')
-     cy.fixture('perfil').then(dados =>{
-         cy.login(dados.usuario, dados.senha)
- });
-})
+    beforeEach(() => {
+        cy.visit('minha-conta')
+        cy.fixture('perfil').then(dados => {
+            cy.login(dados.usuario, dados.senha)
+        })
+    })
 
- it('Deve fazer cadastro de faturamento com sucesso - Usando arquivos de dados', () => {
-     EnderecoPage.editarEndereçoFaturamento(
-         dadosEndereco[1].nome,
-         dadosEndereco[1].sobrenome,
-         dadosEndereco[1].empresa,
-         dadosEndereco[1].pais,
-         dadosEndereco[1].endereco,
-         dadosEndereco[1].numero,
-         dadosEndereco[1].cidade,
-         dadosEndereco[1].estado,
-         dadosEndereco[1].cep,
-         dadosEndereco[1].telefone,
-         dadosEndereco[1].email
-)
-     cy.get('.woocommerce-message').should('contain' , 'Endereço alterado com sucesso')
- });
- 
+    beforeEach(() => {
 
-});
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
 
+
+    it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
+        cy.addproduto('Aether Gym Pant', '34', 'Green', '1')
+        cy.addproduto('Aether Gym Pant', '33', 'Blue', '1')
+        cy.addproduto('Ingrid Running Jacket', 'XS', 'Red', '2')
+        cy.get('#cart > .dropdown-toggle').click()
+        cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .view-cart')
+
+        it('Deve fazer cadastro de faturamento com sucesso - Usando arquivos de dados', () => {
+            EnderecoPage.editarEndereçoFaturamento(
+                dadosEndereco[1].nome,
+                dadosEndereco[1].sobrenome,
+                dadosEndereco[1].empresa,
+                dadosEndereco[1].pais,
+                dadosEndereco[1].endereco,
+                dadosEndereco[1].numero,
+                dadosEndereco[1].cidade,
+                dadosEndereco[1].estado,
+                dadosEndereco[1].cep,
+                dadosEndereco[1].telefone,
+                dadosEndereco[1].email
+            )
+            cy.get('#terms').click()
+            cy.get('#place_order').click()
+            cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso')
+        });
+    })
+})    
